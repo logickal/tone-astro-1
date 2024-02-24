2ec7797389a5a5ad88bfcb05a440def3412602fe
jeremydickens <jeremy@offnominal.com>
2024-02-24 16:31:20 +0000

    4 players with parameters

--- src/pages/experiment-5a/Experiment5a.svelte
+++ src/pages/experiment-5a/Experiment5a.svelte
@@ -34,7 +34,7 @@
 
     let filterTypes = ["none", "lowpass", "highpass", "bandpass", "notch"];
     let modSources = ["none", "envelope1", "envelope2", "lfo1", "lfo2", "lfo3"];
-    let pitchValues = [0, +7, +12, -12, -5];
+    let pitchValues = [1, 1.25, 2, .5, .75, .25];
 
     // We will start with the same 4 samples.
     // We will then generate parameters for them:
@@ -63,7 +63,7 @@
             // loopStart and loopLength will need to be set based on the duration of the file.
             //let loopStart = getRandom(0, 10);
             //let loopLength = getRandom(10, 20);
-            let pitch = getRandom(0.5, 2);
+            let pitch = setRandomParam(pitchValues);
             let filter = getRandom(0, 3);
             let filterFreq = getRandom(100, 1000);
             let filterMod = getRandom(0, 1);
@@ -81,7 +81,7 @@
             let pan = getRandom(-1, 1);
             let panMod = getRandom(0, 1);
             let attack = getRandom(15, 30);
-            let release = (attack + getRandom(15, 30));
+            let release = attack + getRandom(15, 30);
             let effect = setRandomParam(["delay4", "delay2"]);
             let playing = false;
 
@@ -123,6 +123,7 @@
     // - play the second sample for a duration
 
     onMount(async () => {
+        Tone.Transport.debug = true;
         Tone.start();
         Tone.Transport.bpm.value = 55;
 
@@ -132,33 +133,29 @@
             channel3: new Tone.Channel().toDestination(),
             channel4: new Tone.Channel().toDestination(),
             channel5: new Tone.Channel().toDestination(),
-            delay2: new Tone.Channel().toDestination(),
-            delay4: new Tone.Channel().toDestination(),
+            delay2: new Tone.Channel(),
+            delay4: new Tone.Channel(),
             channelReverb: new Tone.Channel().toDestination(),
         };
 
-            // Reverb
-    // Delay4
-    // Delay2
-    const reverb = new Tone.Reverb({
-        decay: 15,
-        preDelay: 0.2,
-        wet: 1,
-    }).connect(channels.channelReverb);
-    const delay4 = new Tone.FeedbackDelay(800, 0.65).set({ wet: 0.5 });
-    const delay2 = new Tone.FeedbackDelay(1600, 0.65).set({ wet: 0.5 });
+        // Reverb
+        // Delay4
+        // Delay2
+        const reverb = new Tone.Reverb({
+            decay: 15,
+            preDelay: 0.2,
+            wet: 1,
+        }).connect(channels.channelReverb);
+        const delay4 = new Tone.FeedbackDelay(1600, 0.65).set({ wet: 0.5 });
+        const delay2 = new Tone.FeedbackDelay(5000, 0.65).set({ wet: 0.5 });
 
-    const filter4 = new Tone.Filter(1000, "lowpass");
-    const filter2 = new Tone.Filter(900, "lowpass");
+        const filter4 = new Tone.Filter(1000, "lowpass");
+        const filter2 = new Tone.Filter(900, "lowpass");
 
-    delay4.connect(filter4);
-    delay2.connect(filter2);
-    filter2.connect(channels.delay2);
-    filter4.connect(channels.delay4);
-    channels.delay2.send('channelReverb', 0.5);
-    channels.delay4.send('channelReverb', 0.5);
-
-
+        delay4.chain(filter4, channels.delay4);
+        delay2.chain(filter2, channels.delay2);
+        channels.delay2.send("channelReverb", 0.5);
+        channels.delay4.send("channelReverb", 0.5);
 
         // Reactive statement that updates messageText based on the value of isPlaying
         isPlaying.subscribe((value) => {
@@ -173,87 +170,92 @@
             let playerState = messageText;
             // Play the piece if isPlaying is true
             if (value === true) {
-                console.log('1. isPlaying is true');
+                console.log("1. isPlaying is true");
                 // Play one voice
                 playPiece(playerParams, 1);
             } else {
                 // Stop the piece.
-                console.log('isPlaying is false');
+                console.log("isPlaying is false");
                 stopPiece();
-                
-            }   
+            }
         });
-
-
     });
 
     // The loop execution for each voice
     function voicePlayback(playerParams, player, envelope) {
-        console.log('5. voicePlayback');
+        console.log("5. voicePlayback");
         // Start the player - schedule NOW and offset 5 seconds into the sample
         // TODO: configure the duration and work out how to get the start time working properly.
         player.start(Tone.now(), 5);
         // Transport should already be started, so start the loop.
-        console.log('6. Loop starting.');
+        console.log("6. Loop starting.");
         // Set the loop interval to the sum of the attack and release plus 1 measure at the current tempo.
-        let interval = envelope.attack + envelope.release + Tone.Transport.toSeconds("1m");
-        console.log('Interval: ' + interval);
+        let interval =
+            envelope.attack + envelope.release + Tone.Transport.toSeconds("1m");
+        console.log("Interval: " + interval);
         let loop = new Tone.Loop((time) => {
             // Trigger the envelope
             Tone.Transport.schedule((time) => {
-                console.log('7. triggerEnvelope');
+                console.log("7. triggerEnvelope for " + playerParams.audioFile);
                 triggerEnvelope(envelope, Tone.now());
-             }, Tone.now());
-         }, interval).start(0);
+            }, Tone.now());
+        }, interval).start(0);
     }
 
     // Function to trigger the envelope
     function triggerEnvelope(envelope, duration) {
-        console.log('8. triggerEnvelope');
-        envelope.triggerAttackRelease((envelope.attack + "8n"), duration);
+        console.log("8. triggerEnvelope");
+        envelope.triggerAttackRelease(envelope.attack + "8n", duration);
     }
 
     function setLoopPoints(player) {
-        console.log('3. setLoopPoints');
-            // Set the loop start and end
-            const duration = player.buffer.duration;
-            const loopLength = Math.min(getRandom(1, 5), duration);
-            const start = Math.random() * (duration - loopLength);
-            player.startPoint = start;
-            player.setLoopPoints(start, start + loopLength);
-            player.start(0, player.startPoint);
-            player.loop = true;
+        console.log("3. setLoopPoints");
+        // Set the loop start and end
+        const duration = player.buffer.duration;
+        const loopLength = Math.min(getRandom(1, 5), duration);
+        const start = Math.random() * (duration - loopLength);
+        player.startPoint = start;
+        player.setLoopPoints(start, start + loopLength);
+        player.start(0, player.startPoint);
+        player.loop = true;
     }
 
     // Play the piece - main player creation and kick off playing
     let playPiece = (playerParams, voices) => {
-        console.log('2. playPiece called');
-        let testAmpEnv = new Tone.AmplitudeEnvelope({
-            attack: playerParams[0].attack,
-            decay: 0.4,
-            sustain: 1,
-            release: playerParams[0].release,
-        }).connect(channels.channel1);
-        channels.channel1.send("channelReverb", .5);
-        channels.channel1.send(`channels.${playerParams[0].effect}`, .5);
+        console.log("2. playPiece called");
 
-        let testPlayer = new Tone.Player(playerParams[0].audioFile, () => {
-            console.log('3. testPlayer callback');
-            setLoopPoints(testPlayer);
-            console.log('4. setLoopPoints: ' +  testPlayer.loopStart);
-            console.log('Voice playback staring');
-            voicePlayback(playerParams, testPlayer, testAmpEnv);
-        }).connect(testAmpEnv);
+        let players = [player1, player2, player3, player4];
+        let ampEnvs = [ampEnv1, ampEnv2, ampEnv3, ampEnv4];
+
+        playerParams.forEach((params, index) => {
+            let channel = channels[`channel${index + 1}`];
+
+            ampEnvs[index] = new Tone.AmplitudeEnvelope({
+                attack: params.attack,
+                decay: 0.4,
+                sustain: 1,
+                release: params.release,
+            }).connect(channel);
+
+            channel.send("channelReverb", 0.8);
+            channel.send(`channels.${params.effect}`, 0.5);
+
+            players[index] = new Tone.Player(params.audioFile, () => {
+                console.log("3. testPlayer callback");
+                setLoopPoints(players[index]);
+                players[index].playbackRate = params.pitch;
+                console.log("4. setLoopPoints: " + players[index].loopStart);
+                console.log("Voice playback staring");
+                voicePlayback(params, players[index], ampEnvs[index]);
+            }).connect(ampEnvs[index]);
+        });
+
         // Start the transport.
         Tone.Transport.start();
-
     };
-
     let stopPiece = () => {
         Tone.Transport.stop();
     };
-
-
 </script>
 
 <div>
